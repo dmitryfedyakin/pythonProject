0dc2c3c56b2eb552bcc4fafd26bdcfb5c9085e1f
dmitryfedyakin <d.fedyakin@g.nsu.ru>
2023-03-02 11:14:44 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+'''
+Title 
+Group:
+Ivanov Ivan
+Petrov Petr
+'''
+
+def triangle(x,y,a,b,angle,color):
+    '''
+    Function for drawing triangle
+    :param x:
+    :param y:
+    :param a:
+    :param b:
+    :param angle:
+    :param color:
+    :return: None
+    '''
+    pass
+
+def draw_dog():
+    pass
+
+def draw_cat():
+    pass
+
+def main():
+    '''
+    Main function
+    :return:
+    '''
+    draw_cat()
+    draw_dog()
+
+if __name__ == '__main__':
+    main()
